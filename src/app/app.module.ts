899745d5c04f3a41e7e6dff97e872519be456223
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2021-10-22 23:06:58 +0300

    ProductDisplayComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductRowComponent } from './product-row/product-row.component';
 import { ProductImageComponent } from './product-image/product-image.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { ProductImageComponent } from './product-image/product-image.component';
     ProductsListComponent,
     ProductRowComponent,
     ProductImageComponent,
+    PriceDisplayComponent,
   ],
   imports: [
     BrowserModule
