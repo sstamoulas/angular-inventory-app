94a4b7c625a90c94925054e8b96cb020d68561bd
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2021-10-22 22:51:20 +0300

    ProductListComponent

---
+++ src/app/products-list/products-list.component.ts
@@ -0,0 +1,45 @@
+import { 
+  Component, 
+  EventEmitter, 
+  Input, 
+  Output
+} from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'products-list',
+  templateUrl: './products-list.component.html'
+})
+export class ProductsListComponent {
+  /*
+   * the Product[] passed to us
+   */
+  @Input() productList: Product[];
+
+  /*
+   * outputs the current Product when a new product is selected 
+   */
+  @Output() onProductSelected: EventEmitter<Product>;
+
+  /*
+   * local state containing the currently selected `Product`
+   */
+  private currentProduct: Product;
+
+  constructor() {
+    this.onProductSelected = new EventEmitter();
+  }
+
+  clicked(product: Product): void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product: Product): boolean {
+    if(!product || !this.currentProduct) {
+      return false;
+    }
+
+    return product.sku === this.currentProduct.sku;
+  }
+}
