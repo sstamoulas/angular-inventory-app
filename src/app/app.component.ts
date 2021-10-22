d9b00c61825f728bfd7fffdb462254e082781b59
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2021-10-22 23:12:48 +0300

    ProductDepartment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,21 +14,21 @@ export class AppComponent {
       new Product(
         'MYSHOES',
         'Black Running Shoes', 
-        '/assets/images/products/black-shoes.jpg', 
+        '/assets/images/products/black-shoes.jpeg', 
         ['Men', 'Shoes', 'Running Shoes'], 
         109.99
       ),
       new Product(
         'NEATOJACKET',
         'Blue Jacket', 
-        '/assets/images/products/blue-jacket.jpg', 
+        '/assets/images/products/blue-jacket.jpeg', 
         ['Women', 'Apparel', 'Jackets & Vests'], 
         238.99
       ),
       new Product(
         'NICEHAT',
         'A Nice Black Hat', 
-        '/assets/images/products/black-hat.jpg', 
+        '/assets/images/products/black-hat.jpeg', 
         ['Men', 'Accessories', 'Hats'],
         29.99
       )
